9424fdc32496b52771aad320d1d4d174eb401e94
Soumadip Das <soumadip377@gmail.com>
2024-11-26 11:57:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,5 +53,5 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         document.getElementById("giftMessage").style.display = "block";
-    }, 9000);
+    },11000);
 });
