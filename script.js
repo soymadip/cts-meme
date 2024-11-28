f55942dfbf38939281530f544fccffd8813b94fd
soymadip <soumadip377@gmail.com>
2024-11-28 22:08:31 +0530

    modify video popup

--- script.js
+++ script.js
@@ -30,26 +30,33 @@ document.addEventListener("DOMContentLoaded", () => {
 	const modal = document.getElementById("gift-modal");
 	const closeButton = document.querySelector(".close-button");
 	const modalVideo = modal.querySelector("video");
+	const giftMessage = document.getElementById("giftMessage");
 
 	giftPopup.addEventListener("click", () => {
 		modal.style.display = "block";
+		modalVideo.currentTime = 0;
+		modalVideo.play();
+		giftMessage.style.display = "none";
+	});
+
+	// Add timeupdate event listener to check video progress
+	modalVideo.addEventListener("timeupdate", () => {
+		if (modalVideo.currentTime >= modalVideo.duration / 2.6) {
+			giftMessage.style.display = "block";
+		}
 	});
 
 	closeButton.addEventListener("click", () => {
 		modal.style.display = "none";
 		modalVideo.pause();
+		modalVideo.currentTime = 0; // Reset video position
 	});
 
 	window.addEventListener("click", (event) => {
 		if (event.target === modal) {
 			modal.style.display = "none";
 			modalVideo.pause();
+			modalVideo.currentTime = 0; // Reset video position
 		}
 	});
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-	setTimeout(() => {
-		document.getElementById("giftMessage").style.display = "block";
-	}, 15000);
 });
